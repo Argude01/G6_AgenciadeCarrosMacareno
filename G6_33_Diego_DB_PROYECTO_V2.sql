c39a919a805c068296719d2cc51349f03d6ba1a5
Diego22032003 <79998888+Diego22032003@users.noreply.github.com>
2021-06-01 16:11:17 -0600

    Add files via upload

---
+++ G6_33_Diego_DB_PROYECTO_V2.sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-05-2021 a las 06:00:23
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_ventas_autos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_autos`
+--
+
+CREATE TABLE `tbl_autos` (
+  `ID_AUTO` int(11) NOT NULL,
+  `MARCA` varchar(50) NOT NULL,
+  `MODELO` varchar(50) NOT NULL,
+  `COLOR` varchar(50) NOT NULL,
+  `AÑO` int(60) NOT NULL,
+  `OBSERVACION` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_clientes`
+--
+
+CREATE TABLE `tbl_clientes` (
+  `ID_CLIENTE` int(11) NOT NULL,
+  `NOMBRE` varchar(50) NOT NULL,
+  `APELLIDO` varchar(50) NOT NULL,
+  `N DE IDENTIDAD` varchar(100) NOT NULL,
+  `RTN` int(100) NOT NULL,
+  `TELEFONO` varchar(50) NOT NULL,
+  `ID_DIRECCION` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_direcciones`
+--
+
+CREATE TABLE `tbl_direcciones` (
+  `ID_DIRECCION` int(11) NOT NULL,
+  `DIRECCION` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tbl_direcciones`
+--
+
+INSERT INTO `tbl_direcciones` (`ID_DIRECCION`, `DIRECCION`) VALUES
+(1, 'BARRIO ARRIBA'),
+(2, 'BARRIO EL CENTRO'),
+(3, 'BARRIO EL CENTRO'),
+(4, 'BARRIO BRISAS DE ALTAMIRA');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_facturas`
+--
+
+CREATE TABLE `tbl_facturas` (
+  `ID_FACTURA` int(11) NOT NULL,
+  `FECHA` date NOT NULL,
+  `ID_DIRECCION` int(11) NOT NULL,
+  `ARTICULOS` varchar(200) NOT NULL,
+  `PRECIO UNITARIO` int(255) NOT NULL,
+  `ISV` int(100) NOT NULL,
+  `TOTAL` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_registros`
+--
+
+CREATE TABLE `tbl_registros` (
+  `ID_REGISTRO` int(11) NOT NULL,
+  `CORREO` varchar(100) NOT NULL,
+  `CLAVE` varchar(50) NOT NULL,
+  `AÑOS` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_autos`
+--
+ALTER TABLE `tbl_autos`
+  ADD PRIMARY KEY (`ID_AUTO`);
+
+--
+-- Indices de la tabla `tbl_clientes`
+--
+ALTER TABLE `tbl_clientes`
+  ADD PRIMARY KEY (`ID_CLIENTE`),
+  ADD KEY `FK_DIRECCIONES_CLIENTES` (`ID_DIRECCION`);
+
+--
+-- Indices de la tabla `tbl_direcciones`
+--
+ALTER TABLE `tbl_direcciones`
+  ADD PRIMARY KEY (`ID_DIRECCION`);
+
+--
+-- Indices de la tabla `tbl_facturas`
+--
+ALTER TABLE `tbl_facturas`
+  ADD PRIMARY KEY (`ID_FACTURA`),
+  ADD KEY `FK_DIRECCIONES_FACTURAS` (`ID_DIRECCION`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_autos`
+--
+ALTER TABLE `tbl_autos`
+  MODIFY `ID_AUTO` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_clientes`
+--
+ALTER TABLE `tbl_clientes`
+  MODIFY `ID_CLIENTE` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_direcciones`
+--
+ALTER TABLE `tbl_direcciones`
+  MODIFY `ID_DIRECCION` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_facturas`
+--
+ALTER TABLE `tbl_facturas`
+  MODIFY `ID_FACTURA` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tbl_clientes`
+--
+ALTER TABLE `tbl_clientes`
+  ADD CONSTRAINT `FK_DIRECCIONES_CLIENTES` FOREIGN KEY (`ID_DIRECCION`) REFERENCES `tbl_direcciones` (`ID_DIRECCION`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tbl_facturas`
+--
+ALTER TABLE `tbl_facturas`
+  ADD CONSTRAINT `FK_DIRECCIONES_FACTURAS` FOREIGN KEY (`ID_DIRECCION`) REFERENCES `tbl_direcciones` (`ID_DIRECCION`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
